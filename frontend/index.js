6e6f5b3375caa4e6d837aeda26aa5211e4312c4b
wafuwafu13 <mariobaske@i.softbank.jp>
2020-06-01 10:19:04 +0900

    connect mysql with node.js

---
+++ frontend/index.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const mysql = require('mysql')
+
+const env = process.env
+
+const connection = mysql.createConnection({
+    host: env.HOST,
+    user: env.USER,
+    database: env.DATABASE,
+    password: env.PASSWORD
+})
+
+connection.connect();
+
+connection.query("select * from users;", function(err, rows, fields) {
+    if (err) {
+        console.log(err)
+    }
+    console.log(rows)
+})
+
+connection.end()
