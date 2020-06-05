f70b1d420a6686097fd871fc9edaefe64930e0ce
wafuwafu13 <mariobaske@i.softbank.jp>
2020-06-05 15:59:22 +0900

    change url

--- server.js
+++ server.js
@@ -8,19 +8,19 @@ app.use(express.static(__dirname + '/public'))
 
 const axiosBase = require('axios');
 const axios = axiosBase.create({
-  baseURL: 'https://agile-thicket-77925.herokuapp.com/api/v1',
+  baseURL: 'https://go-go-pokemon-api-server.herokuapp.com/api/v1',
   headers: {
     'Content-Type': 'application/json',
     'X-Requested-With': 'XMLHttpRequest'
   },
-  responseType: 'json'  
+  responseType: 'json'
 });
 
 app.get('/', (res) => {
   res.sendfile(__dirname + '/public/index.html');
 })
 
-app.get('/pokemon', (res) => {
+app.get('/pokemon', (req, res, next) => {
   axios.get('/pokemons')
   .then((response) => {
     res.render('index', {data: response.data.data})
@@ -31,7 +31,7 @@ app.get('/pokemon', (res) => {
   });
 });
 
-app.get('/post/:name/:id', (req) => {
+app.get('/post/:name/:id', (req, res, next) => {
   axios.post('/pokemons', {
     name: req.params.name,
     poke_id: req.params.id
@@ -44,7 +44,7 @@ app.get('/post/:name/:id', (req) => {
   });
 });
 
-app.get('/delete/:id', (req) => {
+app.get('/delete/:id', (req, res, next) => {
   axios.delete('pokemons/' + req.params.id)
   .then((response) => {
     console.log(response.data)
