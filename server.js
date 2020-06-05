5e38d3e171f4f9621d755a5f01f633c5b3d3e829
wafuwafu13 <mariobaske@i.softbank.jp>
2020-06-05 18:16:10 +0900

    delete commetn

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 
-//app.set('port', (process.env.PORT || 3000));
 app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs')
 
