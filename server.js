5401c1275025a840e3dfe7472b8bea66ac38a124
wafuwafu13 <mariobaske@i.softbank.jp>
2020-06-05 15:24:24 +0900

    delete unused variable

--- server.js
+++ server.js
@@ -16,11 +16,11 @@ const axios = axiosBase.create({
   responseType: 'json'  
 });
 
-app.get('/', (req, res, next) => {
+app.get('/', (res) => {
   res.sendfile(__dirname + '/public/index.html');
 })
 
-app.get('/pokemon', (req, res, next) => {
+app.get('/pokemon', (res) => {
   axios.get('/pokemons')
   .then((response) => {
     res.render('index', {data: response.data.data})
@@ -31,7 +31,7 @@ app.get('/pokemon', (req, res, next) => {
   });
 });
 
-app.get('/post/:name/:id', (req, res, next) => {
+app.get('/post/:name/:id', (req) => {
   axios.post('/pokemons', {
     name: req.params.name,
     poke_id: req.params.id
@@ -44,7 +44,7 @@ app.get('/post/:name/:id', (req, res, next) => {
   });
 });
 
-app.get('/delete/:id', (req, res, next) => {
+app.get('/delete/:id', (req) => {
   axios.delete('pokemons/' + req.params.id)
   .then((response) => {
     console.log(response.data)
